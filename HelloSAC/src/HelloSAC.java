6cc1bac9c9b16cb48247a41d2f66b6527b5c7696
SAC-CS112-Payan-Luis <luis_gohan@yahoo.com>
2017-02-25 23:43:28 -0800

    Add files via upload

---
+++ HelloSAC/src/HelloSAC.java
@@ -0,0 +1,12 @@
+/*Luis Payan
+ * Professor Durendal
+ * Wed @ 6:30
+ * Hello World application*/
+
+public class HelloSAC {
+	//main method begins to execution of Java application
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello SAC!");
+	}//end method main
+}// end class HelloSAC
